fe745a5dcc4b442971861ddcba33a83c799e7ba5
Nicolai Dreves Nielsen <Nicolai_Dreves1994@hotmail.dk>
2020-05-11 14:57:56 +0200

    made obs controller

---
+++ VejrStation/VejrStation/Controllers/ObservationsController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VejrStation.Database;
+using VejrStation.Entities;
+
+namespace VejrStation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ObservationsController : ControllerBase
+    {
+        private readonly MyDBContext _context;
+
+        public ObservationsController(MyDBContext context)
+        {
+            _context = context;
+        }
+
+        //get to see all the observations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Observation>>> GetObservations()
+        {
+            return await _context.Observations.ToListAsync();
+        }
+
+        //Get to see the oberservation specified by a specific id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Observation>> GetObservation(long id)
+        {
+            var observation = await _context.Observations.FindAsync(id);
+
+            if (observation == null)
+            {
+                return NotFound();
+            }
+
+            return observation;
+        }
+
+        //get to see all the observations for a specified date
+        [HttpGet("{DateObserved}")]
+        public async Task<ActionResult<object>> GetObservationsDate(DateTime date)
+        {
+            List<Observation> observations = _context.Observations
+                .Where(a => a.DateObserved.Date == date.Date)
+                .ToList();
+
+            if (observations[0] == null) //If there is no observations on the first element in the array return notfound
+            {
+                return NotFound();
+            }
+
+            return observations;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Observation>> CreateObservation(Observation observation)
+        {
+            _context.Observations.Add(observation);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtRoute("GetObservation", new { id = observation.Id }, observation);
+        }
+
+    }
+}
---
+++ VejrStation/VejrStation/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VejrStation.Database;
+using VejrStation.Entities;
+
+namespace VejrStation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly MyDBContext _context;
+
+        public UsersController(MyDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(long id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtRoute("GetUser", new {id = user.Id}, user);
+        }
+
+
+    }
+}
