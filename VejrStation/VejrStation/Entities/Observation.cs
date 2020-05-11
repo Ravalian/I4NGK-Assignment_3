072ef8fdfa730f4c6a268ac4d603306e40152a6e
Nicolai Dreves Nielsen <Nicolai_Dreves1994@hotmail.dk>
2020-05-11 10:59:23 +0200

    S

---
+++ VejrStation/VejrStation/Entities/Observation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Threading.Tasks;
+
+namespace VejrStation.Entities
+{
+    public class Observation
+    {
+        public DateTime DateObserved { get; set; }  //The date and time the observation were made
+        public string locationName { get; set; }    //The location name for the observation
+        public string locationLat { get; set; }     //gps coordinates Latitude
+        public string locationLot { get; set; }     //gps coordinates Longitude
+        public float Temperature { get; set; }      //should have 1 decimals accuracy
+        public int Humidity { get; set; }           //Humidity in integers
+        public float AirPressure { get; set; }      //Should have 1 decimals accuracy
+    }
+}
