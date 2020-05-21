4d75c6d3a13328f247d2c3c106751c2e9504060d
Warmdahl <emilwarmdahl@gmail.com>
2020-05-21 22:52:37 +0200

    Commit

---
+++ VejrStation/VejrStation/TokenDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VejrStation
+{
+    public class TokenDto
+    {
+        public string JWT { get; set; }
+    }
+}
